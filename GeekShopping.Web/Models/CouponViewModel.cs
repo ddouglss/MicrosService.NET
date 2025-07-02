27b6f98c0052b4d7ea3a69e8a1157027603f412a
ddouglss <ddouglss1999@gmail.com>
2025-07-01 22:36:36 -0300

    feat(models) Implementando o microserviços de Cupom de Descontos

---
+++ GeekShopping.Web/Models/CouponViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class CouponViewModel
+    {
+        public long Id { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+
+        public CouponViewModel()
+        {
+            
+        }
+    }
+}
--- GeekShopping.Web/obj/Debug/net8.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping.Web/obj/Debug/net8.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a6d47d7cb51a2836853c07e85b2bc37eac16bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f34e5973ffadedf9b463ea549b54764e446d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
